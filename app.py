c7bd72ad4763ac53cd384eeefd426da267d3495e
Johan Santallana <johan921229@gmail.com>
2026-02-25 21:41:48 -0500

    Add database initialization in app context

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gundams.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
 with app.app_context():
     db.create_all()
 
